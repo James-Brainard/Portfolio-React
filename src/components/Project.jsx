18bedbe417da298ed630d302c84c064c3152fb6d
James Brainard <james.brainard2@aol.com>
2023-10-29 15:39:08 -0700

    fixed bs styling for portfolio page to only display 2 projects per row.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -5,23 +5,27 @@ import Button from 'react-bootstrap/Button';
 
 function AboutProjects({ projects }) {
   return (
-    <div className="d-flex justify-content-between flex-wrap">
+    <div className="container">
+      <div className="row">
       {projects.map((project) => {
         const { title, githubLink, deployedLink, image, description } = project;
         return (
-          <Card className="row mb-5 mt-3" style={{ width: '18rem', border: '1px white solid', background: 'black' }} key={title}>
-            <Card.Img variant="top" src={image} />
-            <Card.Body>
-              <Card.Title>{title}</Card.Title>
-              <Card.Text>
-                {description}
-              </Card.Text>
-              <Button className="mb-2" href={deployedLink} target="_blank" variant="primary">View App</Button>
-              <Button href={githubLink} target="_blank" variant="secondary">View Repo</Button>
-            </Card.Body>
-          </Card>
+          <div className="col-md-6 col-lg-6 d-flex justify-content-center">
+            <Card className="mb-5 mt-3" style={{ width: '18rem', border: '1px white solid', background: 'black' }} key={title}>
+              <Card.Img variant="top" src={image} />
+              <Card.Body>
+                <Card.Title>{title}</Card.Title>
+                <Card.Text>
+                  {description}
+                </Card.Text>
+                <Button className="mb-2" href={deployedLink} target="_blank" variant="primary">View App</Button>
+                <Button href={githubLink} target="_blank" variant="secondary">View Repo</Button>
+              </Card.Body>
+            </Card>
+          </div>
         );
       })}
+      </div>
     </div>
   );
 }
